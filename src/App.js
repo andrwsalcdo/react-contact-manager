b5dea3c3c8bbbaadad2cf82f82a4a1c0711b9d0f
Andrew Salcedo <asalcedo07@gmail.com>
2017-09-15 10:55:25 -0400

    Add <Header /> with bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        hello world
+        <Header />
       </div>
     );
   }
